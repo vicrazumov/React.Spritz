7614450f327503a179c7106a39b263754c4b1e10
vicrazumov <vnrazumov@gmail.com>
2017-11-15 21:00:15 +0300

    [Feature] emoji support added

--- src/ReactSpritz.js
+++ src/ReactSpritz.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ReactSpritz.scss';
 
-const calcHighlightPoint = word => Math.floor(word.replace(/[\W]/g, '').length / 2);
-const textToWords = text => text.replace(/^\s+|\s+|\n$/, '').split(/\s+/);
+const isEmoji = word => /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g.test(word)
+const calcHighlightPoint = word => isEmoji(word) ? -1 : Math.floor(word.replace(/[\W]/g, '').length / 2);
+const textToWords = text => text.split(/\s+/);
 
 class ReactSpritz extends React.Component {
   constructor(props) {
@@ -63,7 +64,7 @@ class ReactSpritz extends React.Component {
 
     if (index === this.words.length - 1) this.onStop();
 
-    this.delay = /^\(|[,\.\)]$/.test(word);
+    this.delay = /^\(|[,\.\)]$/.test(word) || isEmoji(word);
   }
 
   _start = () => {
@@ -116,13 +117,13 @@ class ReactSpritz extends React.Component {
           word &&
           <div className="spritz">
             <div className="leftSide">
-              {word.slice(0, highlightIndex)}
+              {highlightIndex !== -1 && word.slice(0, highlightIndex)}
             </div>
             <div className="highlight">
-              {word[highlightIndex]}
+              {highlightIndex === -1 ? word : word[highlightIndex]}
             </div>
             <div className="rightSide">
-              {word.slice(highlightIndex + 1)}
+              {highlightIndex !== -1 && word.slice(highlightIndex + 1)}
             </div>
           </div>
         }
