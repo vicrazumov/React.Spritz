08b65e18480457f6ba685514ac544ffd58df2e9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-06 20:33:14 +0000

    Deploy vicrazumov/React.Spritz to github.com/vicrazumov/React.Spritz.git:gh-pages

---
+++ precache-manifest.f3c0c3b2544facb18591d1b0970c554e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "340e0f932ab92a593903",
+    "url": "./static/css/main.34669a48.chunk.css"
+  },
+  {
+    "revision": "340e0f932ab92a593903",
+    "url": "./static/js/main.340e0f93.chunk.js"
+  },
+  {
+    "revision": "53ebf1fc8c2b7d4855e0",
+    "url": "./static/css/1.4e3209cd.chunk.css"
+  },
+  {
+    "revision": "53ebf1fc8c2b7d4855e0",
+    "url": "./static/js/1.53ebf1fc.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5b8403065bc4e394bdaa295826461e13",
+    "url": "./index.html"
+  }
+];
